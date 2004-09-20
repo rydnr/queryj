15a86a5b0f6717fce0cd297c0fb6d78587d55d5b
chous <chous@5eb9b703-b319-0410-90d6-a93288512442>
2004-09-20 19:25:28 +0000

    Added missing file.

---
+++ src/java/main/org/acmsl/queryj/tools/templates/handlers/TemplateHandler.java
@@ -0,0 +1,81 @@
+/*
+                        QueryJ
+
+    Copyright (C) 2002  Jose San Leandro Armendáriz
+                        jsanleandro@yahoo.es
+                        chousz@yahoo.com
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+    You should have received a copy of the GNU General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+    Thanks to ACM S.L. for distributing this library under the GPL license.
+    Contact info: jsanleandro@yahoo.es
+    Postal Address: c/Playa de Lagoa, 1
+                    Urb. Valdecabañas
+                    Boadilla del monte
+                    28660 Madrid
+                    Spain
+
+ ******************************************************************************
+ *
+ * Filename: $RCSfile$
+ *
+ * Author: Jose San Leandro Armendáriz
+ *
+ * Description: Marks all template handlers.
+ *
+ * Last modified by: $Author$ at $Date$
+ *
+ * File version: $Revision$
+ *
+ * Project version: $Name$
+ *
+ * $Id$
+ *
+ */
+package org.acmsl.queryj.tools.templates.handlers;
+
+/*
+ * Importing some project classes.
+ */
+import org.acmsl.queryj.tools.AntCommand;
+
+/*
+ * Importing some ACM-SL classes.
+ */
+import org.acmsl.commons.patterns.CommandHandler;
+
+/*
+ * Importing some Ant classes.
+ */
+import org.apache.tools.ant.BuildException;
+
+/**
+ * Marks all template handlers.
+ * @author <a href="mailto:jsanleandro@yahoo.es"
+           >Jose San Leandro</a>
+ * @version $Revision$ at $Date$
+ */
+public interface TemplateHandler
+    extends CommandHandler
+{
+    /**
+     * Marks all template handlers.
+     * @param command the command to handle.
+     * @return <code>true</code> if the chain should be stopped.
+     * @throws BuildException if the build process cannot be performed.
+     */
+    public boolean handle(final AntCommand command)
+        throws  BuildException;
+}
