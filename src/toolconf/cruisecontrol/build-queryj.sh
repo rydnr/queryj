63f412815e94981e84a480433b7a55aa9153e5d3
chous <chous@5eb9b703-b319-0410-90d6-a93288512442>
2006-03-22 08:15:20 +0000

    Added Ant delegating script for CruiseControl

--- src/toolconf/cruisecontrol/build-queryj.sh
+++ src/toolconf/cruisecontrol/build-queryj.sh
@@ -4,6 +4,7 @@ usage() {
     echo "Usage $0 [trunk|<branch>]"
     echo "Checks out the trunk or a given branch of Queryj,"
     echo "and builds it afterwards."
+    echo "Note: this script tries to delete any already-existing checkout."
     exit 1;
 }
 
@@ -14,4 +15,13 @@ folder=$1
 if  [[ ! "x$folder" == "xtrunk" ]]; then
   folder=branches/$folder
 fi
-echo "svn co svn://svn.acm-sl.org/var/svn/queryj/$folder" | sh
+if [[ -d $folder ]] ; then
+  if [[ -d previous_build ]] ; then
+    rm -rf previous_build
+  fi
+  if [[ ! -d previous_build ]] ; then
+    mv $folder previous_build
+  fi
+fi
+
+svn co svn://svn.acm-sl.org/var/svn/queryj/$folder >> /dev/null
