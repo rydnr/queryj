294999e5a1d0b32870e6dd20643bd7b39ad25742
chous <chous@5eb9b703-b319-0410-90d6-a93288512442>
2006-03-22 08:41:19 +0000

    Improved build script

--- build.sh
+++ build.sh
@@ -9,17 +9,21 @@ usage () {
 
 [ -z "$1" ] && usage
 
+basedir=`pwd`
+if [[ ! -z "$_DIR" ]]; then
+  basedir=$_DIR
+fi
 if  [[ "x$1" == "x-d" ]]; then
 shift
 [ -z "$1" ] && usage
-for last_command in "cd src/toolconf/maven && mkdir -p ../emacs 2> /dev/null" "cd src/toolconf/maven && maven $ACM_OPTS antdep"; do
+for last_command in "cd ${basedir}/src/toolconf/maven && mkdir -p ../emacs 2> /dev/null" "cd ${basedir}/src/toolconf/maven && maven $ACM_OPTS antdep"; do
   echo "$last_command" | sh
   if [[ ! $? == 0 ]]; then
     exit 1
   fi
 done;
 fi
-for last_command in "cd src/toolconf/ant && ant -f dependencies.xml" "cd src/toolconf/ant && ant $*"; do
+for last_command in "cd ${basedir}/src/toolconf/ant && ant -f dependencies.xml" "cd ${basedir}/src/toolconf/ant && ant $*"; do
   echo "Executing $last_command"
   echo "$last_command" | sh
   if [[ ! $? == 0 ]]; then
