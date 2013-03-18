88d7871a3b2e0f969c067a6caff48d7b5038e1cd
chous <chous@acm-sl.org>
2013-03-18 23:54:57 +0100

    Added fillhandlers for conditionally disable timestamps, @NotNull/@Nullable and @ThreadSafe annotations. refs #364, #365, #366

---
+++ src/main/java/org/acmsl/queryj/templates/handlers/fillhandlers/UseNotNullAnnotationsHandler.java
@@ -0,0 +1,94 @@
+/*
+                        QueryJ
+
+    Copyright (C) 2002-today  Jose San Leandro Armendariz
+                              chous@acm-sl.org
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+    You should have received a copy of the GNU General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+    Thanks to ACM S.L. for distributing this library under the GPL license.
+    Contact info: jose.sanleandro@acm-sl.com
+
+ ******************************************************************************
+ *
+ * Filename: UseNotNullAnnotationsHandler.java
+ *
+ * Author: Jose San Leandro Armendariz (chous)
+ *
+ * Description: Is able to resolve "notnull?" placeholders in
+ *              templates.
+ *
+ */
+package org.acmsl.queryj.templates.handlers.fillhandlers;
+
+/*
+ * Importing project classes.
+ */
+import org.acmsl.queryj.templates.TemplateContext;
+
+/*
+ * Importing some JetBrains annotations.
+ */
+import org.jetbrains.annotations.NotNull;
+
+/*
+ * Importing checkthread.org annotations.
+ */
+import org.checkthread.annotations.ThreadSafe;
+
+/**
+ * Is able to resolve "notnull?" placeholders in templates.
+ * @author <a href="mailto:chous@acm-sl.org">chous</a>
+ * @since 2013/03/18
+ */
+@SuppressWarnings("unused")
+@ThreadSafe
+public class UseNotNullAnnotationsHandler
+    extends AbstractTemplateContextFillHandler<TemplateContext, Boolean>
+{
+    /**
+     * Creates a {@link org.acmsl.queryj.templates.handlers.fillhandlers.UseNotNullAnnotationsHandler} to resolve placeholders
+     * using given {@link org.acmsl.queryj.templates.TemplateContext context}.
+     * @param context the {@link org.acmsl.queryj.templates.TemplateContext context}.
+     */
+    @SuppressWarnings("unused")
+    public UseNotNullAnnotationsHandler(@NotNull final TemplateContext context)
+    {
+        super(context);
+    }
+
+    /**
+     * Returns "notnull?".
+     * @return such placeholder.
+     */
+    @NotNull
+    @Override
+    public String getPlaceHolder()
+    {
+        return "notnull?";
+    }
+
+    /**
+     * Retrieves whether to use NotNull annotations in templates.
+     * @param context the {@link TemplateContext}.
+     * @return such information.
+     */
+    @NotNull
+    @Override
+    protected Boolean getValue(@NotNull final TemplateContext context)
+    {
+        return !context.getDisableNotNullAnnotations();
+    }
+}
