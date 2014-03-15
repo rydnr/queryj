bb09d12b1c409d26aa911e7cadeae4ec4b856874
chous <chous@acm-sl.org>
2014-03-15 10:16:48 +0100

    Added handler to create the PreparedStatement for the current SQL being validated. refs #454

---
+++ queryj-core/src/main/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/SetupPreparedStatementHandler.java
@@ -0,0 +1,181 @@
+/*
+                        QueryJ Core
+
+    Copyright (C) 2002-today  Jose San Leandro Armendariz
+                              chous@acm-sl.org
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+    You should have received a copy of the GNU General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+    Thanks to ACM S.L. for distributing this library under the GPL license.
+    Contact info: jose.sanleandro@acm-sl.com
+
+ ******************************************************************************
+ *
+ * Filename: SetupPreparedStatementHandler.java
+ *
+ * Author: Jose San Leandro Armendariz
+ *
+ * Description: Creates the PreparedStatement for the current Sql and
+ *              "publishes" it into the command.
+ *
+ * Date: 2014/03/15
+ * Time: 10:02
+ *
+ */
+package org.acmsl.queryj.customsql.handlers.customsqlvalidation;
+
+/*
+ * Importing ACM-SL Java Commons classes.
+ */
+import org.acmsl.commons.logging.UniqueLogFactory;
+
+/*
+ * Importing QueryJ Core classes.
+ */
+import org.acmsl.queryj.QueryJCommand;
+import org.acmsl.queryj.QueryJCommandWrapper;
+import org.acmsl.queryj.api.exceptions.InvalidCustomSqlException;
+import org.acmsl.queryj.api.exceptions.QueryJBuildException;
+import org.acmsl.queryj.customsql.Sql;
+import org.acmsl.queryj.customsql.handlers.CustomSqlValidationHandler;
+import org.acmsl.queryj.tools.handlers.AbstractQueryJCommandHandler;
+
+/*
+ * Importing Apache Commons Logging classes.
+ */
+import org.apache.commons.logging.Log;
+
+/*
+ * Importing JetBrains annotations.
+ */
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+/*
+ * Importing checkthread.org annotations.
+ */
+import org.checkthread.annotations.ThreadSafe;
+
+/*
+ * Importing JDK classes.
+ */
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ * Creates the {@link PreparedStatement} for the current {@link Sql} and "publishes" it
+ * into the command.
+ * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
+ * @since 3.0
+ * Created: 2014/03/15 10:02
+ */
+@ThreadSafe
+public class SetupPreparedStatementHandler
+    extends AbstractQueryJCommandHandler
+{
+    /**
+     * The current prepared statement.
+     */
+    @NotNull
+    public static String CURRENT_PREPARED_STATEMENT = "current-prepared-statement";
+
+    /**
+     * Asks the handler to process the command. The idea is that each
+     * command handler decides if such command is suitable of being
+     * processed, and if so perform the concrete actions the command
+     * represents.
+     *
+     * @param command the command to process (or not).
+     * @return <code>true</code> if the handler actually process the command,
+     *         or maybe because it's not desirable to continue the chain.
+     */
+    @Override
+    public boolean handle(@NotNull final QueryJCommand command)
+        throws QueryJBuildException
+    {
+        @NotNull final Sql<String> sql = new RetrieveQueryHandler().retrieveCurrentSql(command);
+        @NotNull final Connection connection = retrieveConnection(command);
+
+        try
+        {
+            @Nullable final PreparedStatement t_Statement = setupStatement(sql, connection);
+
+            setPreparedStatement(t_Statement, command);
+        }
+        catch (@NotNull final SQLException invalidSql)
+        {
+            throw new InvalidCustomSqlException(sql, invalidSql);
+        }
+
+        return false;
+    }
+
+    /**
+     * Annotates the prepared statement into the command.
+     * @param statement the statement.
+     * @param command the command.
+     */
+    protected void setPreparedStatement(final PreparedStatement statement, final QueryJCommand command)
+    {
+        new QueryJCommandWrapper<PreparedStatement>(command).setSetting(CURRENT_PREPARED_STATEMENT, statement);
+    }
+
+    /**
+     * Retrieves the prepared statement from the command.
+     * @param command the command.
+     * @return the statement.
+     */
+    protected PreparedStatement getPreparedStatement(final QueryJCommand command)
+    {
+        return new QueryJCommandWrapper<PreparedStatement >(command).getSetting(CURRENT_PREPARED_STATEMENT);
+    }
+
+    /**
+     * Validates given sql element.
+     * @param sql such element.
+     * @param connection the connection.
+     * @throws SQLException if the sql is not valid.
+     */
+    @Nullable
+    public PreparedStatement setupStatement(
+        @NotNull final Sql<String> sql,
+        @NotNull final Connection connection)
+        throws  SQLException
+    {
+        @Nullable final PreparedStatement result;
+
+        @Nullable final String t_strValue = sql.getValue();
+
+        @Nullable final Log t_Log = UniqueLogFactory.getLog(CustomSqlValidationHandler.class);
+
+        if (t_strValue != null)
+        {
+            @NotNull final String t_strSql = t_strValue.trim();
+
+            result = connection.prepareStatement(t_strSql);
+        }
+        else
+        {
+            if (t_Log != null)
+            {
+                t_Log.warn("Non-select/empty query with validate=\"true\": " + sql.getId());
+            }
+            result = null;
+        }
+
+        return result;
+    }
+}
---
+++ queryj-core/src/test/java/org/acmsl/queryj/customsql/handlers/customsqlvalidation/SetupPreparedStatementHandlerTest.java
@@ -0,0 +1,125 @@
+/*
+                        QueryJ Core
+
+    Copyright (C) 2002-today  Jose San Leandro Armendariz
+                              chous@acm-sl.org
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+    You should have received a copy of the GNU General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+    Thanks to ACM S.L. for distributing this library under the GPL license.
+    Contact info: jose.sanleandro@acm-sl.com
+
+ ******************************************************************************
+ *
+ * Filename: SetupPreparedStatementHandlerTest.java
+ *
+ * Author: Jose San Leandro Armendariz
+ *
+ * Description: Tests for SetupPreparedStatementHandler.
+ *
+ * Date: 2014/03/15
+ * Time: 09:56
+ *
+ */
+package org.acmsl.queryj.customsql.handlers.customsqlvalidation;
+
+/*
+ * Importing QueryJ Core classes.
+ */
+import org.acmsl.queryj.ConfigurationQueryJCommandImpl;
+import org.acmsl.queryj.QueryJCommand;
+import org.acmsl.queryj.QueryJCommandWrapper;
+import org.acmsl.queryj.api.exceptions.QueryJBuildException;
+import org.acmsl.queryj.customsql.Sql;
+import org.acmsl.queryj.customsql.Sql.Cardinality;
+import org.acmsl.queryj.customsql.SqlElement;
+import org.acmsl.queryj.tools.handlers.JdbcConnectionOpeningHandler;
+
+/*
+ * Importing Apache Commons Configuration classes.
+ */
+import org.apache.commons.configuration.PropertiesConfiguration;
+
+/*
+ * Importing JetBrains annotations.
+ */
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+/*
+ * Importing JUnit classes.
+ */
+import org.easymock.EasyMock;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.powermock.api.easymock.PowerMock;
+
+/*
+ * Importing JDK classes.
+ */
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ * Tests for {@link SetupPreparedStatementHandler}
+ * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
+ * @since 3.0
+ * Created: 2014/03/15 09:56
+ */
+@RunWith(JUnit4.class)
+public class SetupPreparedStatementHandlerTest
+{
+    @Test
+    public void injects_a_prepared_statement_into_the_command()
+        throws QueryJBuildException, SQLException
+    {
+        @NotNull final SetupPreparedStatementHandler instance = new SetupPreparedStatementHandler();
+
+        @NotNull final QueryJCommand parameters = new ConfigurationQueryJCommandImpl(new PropertiesConfiguration());
+
+        @NotNull final SqlElement<String> sql =
+            new SqlElement<>(
+                "id", "dao", "name", "String", Cardinality.SINGLE, "all", true /* validation */, false, "description");
+
+        sql.setValue("select sysdate from dual where ? = 'A'");
+
+        new QueryJCommandWrapper<Sql<String>>(parameters).setSetting(RetrieveQueryHandler.CURRENT_SQL, sql);
+
+        @NotNull final Connection t_Connection = PowerMock.createNiceMock(Connection.class);
+        @NotNull final PreparedStatement t_Statement = PowerMock.createNiceMock(PreparedStatement.class);
+
+        EasyMock.expect(t_Connection.prepareStatement(sql.getValue())).andReturn(t_Statement);
+
+        new QueryJCommandWrapper<Connection>(parameters).setSetting(
+            JdbcConnectionOpeningHandler.JDBC_CONNECTION, t_Connection);
+
+        EasyMock.replay(t_Connection);
+
+        Assert.assertFalse(instance.handle(parameters));
+
+        @Nullable final PreparedStatement t_ActualStatement =
+            new QueryJCommandWrapper<PreparedStatement>(parameters).getSetting(
+                SetupPreparedStatementHandler.CURRENT_PREPARED_STATEMENT);
+
+        Assert.assertNotNull(t_ActualStatement);
+        Assert.assertEquals(t_Statement, t_ActualStatement);
+        Assert.assertEquals(t_Statement, instance.getPreparedStatement(parameters));
+
+        EasyMock.verify(t_Connection);
+    }
+}
